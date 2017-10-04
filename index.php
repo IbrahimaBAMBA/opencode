735ddbed21497cdc7482f3465344bdca2b0b3dfb
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-04 09:43:00 +0200

    base de travail

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>openCode</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <header>
+      <?php include 'header.php'; ?>
+    </header>
+    <footer>
+      <?php include 'footer.php' ?>
+    </footer>
+  </body>
+</html>
