011adc2a62e133b180cfb8a55a8f1836d45ce564
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-05 13:51:34 +0200

    correction des conflits header et correction du css

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
-
+<div id="Bottomdiv">
+<div id="FAQ"/><a href="">F.A.Q</a></div>
+<div id="Contacts"/><a href="">Contacts</a></div>
+<div id="Témoignages"/><a href="">Témoignages</a></div>
+</div>
--- header.php
+++ header.php
@@ -1,6 +1,8 @@
-<div class="header">
-
-</div>
-<div class="header">
-
+<div class="topnav" id="myTopnav">
+  <a class="active" href="#home">Accueil</a>
+  <a href="#catalog">catalogue</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">Qui sommes nous ?</a>
+  <a class="nav-right" href="#inscription">Inscription</a>
+  <a class ="nav-right" href="#connection">Connection</a>
 </div>
