28856522bf3f115ee0bd20b4c1e693f24d5969ee
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-05 16:24:09 +0200

    homePage finale

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <div class="topnav" id="myTopnav">
-  <a class="active" href="#home">Accueil</a>
-  <a href="catalogue.php">catalogue</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">Qui sommes nous ?</a>
+  <a class="active left" href="#home">Accueil</a>
+  <a class="" href="catalogue.php">catalogue</a>
+  <a class="" href="#contact">Contact</a>
+  <a class="" href="#about">Qui sommes nous ?</a>
   <a class="nav-right" href="#inscription">Inscription</a>
   <a class ="nav-right" href="#connection">Connection</a>
 </div>
